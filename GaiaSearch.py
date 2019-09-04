9a3462ab02c786a1a886bcf1ab9f89b82754883d
Jeffrey D Cummings <54191136+jeffnogo@users.noreply.github.com>
2019-09-04 16:18:01 -0400

    Add files via upload

--- GaiaSearch.py
+++ GaiaSearch.py
@@ -11,17 +11,17 @@ warnings.filterwarnings("ignore", module='astropy.io.votable.converters')
 warnings.filterwarnings("ignore", module='astropy.extern.six')
 
 #The result outputs a circular search of radius (degrees).  Note: Radii larger than 1.5 
-#degrees typically times out and the DR2 website must be used directly.
+#degrees typically time out and the DR2 website must be used directly.
 radius=0.3
 
-#Cut all targets from output that have a parallax (mas) below parallax_cut or a ratio below parallax_error_ratio
+#Cut all targets from output that have a parallax (mas) below parallax_cut or a error ratio below parallax_error_ratio
 parallax_cut=1
 parallax_error_ratio=3
 
 #The file name, which includes the radius to differentiate variable search sizes.
 Filename="n2547gaiafield"+str(radius)+".csv"
 
-#The cluster name that will be search for position in SIMBAD
+#The cluster name that will be searched for in SIMBAD to find its central coordinates.
 Objectname="NGC 2547"
 
 #Function for converting RA and DEC in H:M:S (or H:M) to decimal degrees
@@ -43,7 +43,7 @@ def coordconv(RAobj,DECobj):
 	print("RA = "+str(round(RAset,4))+" DEC = "+str(round(DECset,4)))
 	return RAset,DECset
 
-#Checks if file already exists, if not, continues search for Simbad coordinates of Objectname and outputs it for conversion.
+#Checks if file already exists, if not, continues search for SIMBAD coordinates of Objectname and outputs it for conversion.
 if not path.exists(Filename):
 	from astroquery.simbad import Simbad
 	
@@ -53,7 +53,7 @@ if not path.exists(Filename):
 	DECobj=result_table["DEC"][0].split()
 	RAset,DECset=coordconv(RAobj,DECobj)
 		
-#If you want to define RA and DEC directly, put them here (degrees)
+#If you want to define RA and DEC directly, uncomment them and define them here (degrees)
 	#RAset=116
 	#DECset=-38
 	
@@ -63,7 +63,7 @@ if not path.exists(Filename):
 # optional: use your API token to use your account
 	#vo.utils.http.session.headers['Authorization'] = 'Token input'
 	
-#Search Gaia database and return to dataframe of Filename the columns of interest for a circular search of defined radius and parallax cuts around ObjectName.
+#Search Gaia database and return to dataframe (Filename) the columns of interest for a circular search of defined radius and parallax cuts around ObjectName.
 	tap_result = tap_service.search("SELECT gaia_source.ra,gaia_source.dec,gaia_source.parallax,gaia_source.parallax_error,gaia_source.pmra,gaia_source.pmra_error,gaia_source.pmdec,gaia_source.pmdec_error,gaia_source.phot_g_mean_mag,gaia_source.bp_rp FROM gdr2.gaia_source WHERE 1=CONTAINS(POINT('ICRS', ra, dec),CIRCLE('ICRS',"+str(RAset)+","+str(DECset)+","+str(radius)+")) AND gaia_source.parallax>="+str(parallax_cut)+" AND gaia_source.parallax_over_error>="+str(parallax_error_ratio))
 	    
 	tap_result.to_table().to_pandas().to_csv(Filename)
