8d7f2307883e28eedb9a4c9783fb7adf81044a3d
Jeffrey D Cummings <54191136+jeffnogo@users.noreply.github.com>
2019-09-09 05:56:27 -0400

    Add files via upload

--- ClusterHdbscan.py
+++ ClusterHdbscan.py
@@ -4,17 +4,22 @@ import pandas as pd
 from clusterplot import clusterplot
 
 #This selects which output cluster data the program will write to a csv file for subsequent analysis.  The star cluster of interest is 
-#commonly "0", but it can sometimes be a variety of numbers depending on density of clusters in the field.
+#commonly "0", but it can sometimes be a variety of numbers depending on the number of clusters in the field.
 extractnum=0
+
+#This flag determines whether or not to add a colormap representing each star's cluster membership probability to the final cluster plots.
+#Enter 1 for yes and 0 (or anything that isn't 1) for no.  Plotting probabilities is recommended, but when probability information
+#isn't critical and simpler/easier to read figures are desired, consider not adding the information.
+PlotMembershipProb=1
 
 #The cluster name identifies the input file (usually using some type of shorthand) and output cluster file names.
 clustername="velaOB2"
 
 #Read the input csv file (from GaiaSearch.py) into a dataframe.  The format of the name is clustername+"gaiafield"+radius size of Gaia 
 #search in degrees+".csv".
-clusterfull=pd.read_csv(clustername+"gaiafield2.5.csv",delimiter=",")
+clusterfull=pd.read_csv(clustername+"gaiafield2.csv",delimiter=",")
 
-#A second dataframe is created which only contains the parameters that will be used to base the HDBSCAN clustering on and analyze the 
+#A second dataframe is created that only contains the parameters that will be used to base the HDBSCAN clustering on and for analyzing the 
 #results; stars that are missing a parameter are dropped.  
 fieldparfull=clusterfull[["ra","dec","pmra","pmdec","parallax","phot_g_mean_mag","bp_rp"]]
 fieldpar=fieldparfull.dropna().copy()
@@ -22,8 +27,9 @@ fieldpar=fieldparfull.dropna().copy()
 #A new distance (pc) column is created in the dataframe based on the observed parallax.  A 0.03 zeropoint correction is applied.
 fieldpar['distance']=1/((fieldpar['parallax']+0.03)*0.001)
 
-#A deep copy of the dataframe is created, I then apply several checks to wrap around the RAs together in the case where they fall 
-#around RA=0,360.
+#A deep copy of the dataframe is created, I then apply several checks/transformations to wrap around the RAs together in the case 
+#where they fall around RA=0,360.  Note that this program may have some difficulty around the poles, but there are no clusters to analyze
+#in those regions.
 fieldparscaled = fieldpar.copy()
 ramax=max(fieldpar["ra"])
 ramin=min(fieldpar["ra"])
@@ -52,9 +58,10 @@ fieldparscaled['pmdec'] = fieldpar['pmdec']*10
 
 #The scaled parameters are applied to the hdbscan function.  A minimum cluster and sample size are set.  64 is typically an appropriate 
 #min_samples for a 5-dimensional hdbscan, but I recommend looking at potential differences in output when this parameter is varied by up 
-#to a factor of 2.  Especially adjust min_samples if a clear cluster in PM space is missed in the output.  Note that we can also use G 
-#magnitude and BP-RP color to increase clustering accuracy.  They are commented out in this input list, but they can help to create a 
-#cleaner cluster main sequence, but at the expense of removing many cluster giants/subdwarfs/white dwarfs.
+#to a factor of 2 (if not more).  Especially adjust min_samples if a clear cluster in PM space is missed in the output.  A smaller 
+#min_samples increases noise but helps differentiate from the field the clusters with proper motions comparable to the field.  Note that 
+#we can also use G magnitude and BP-RP color to increase clustering accuracy.  They are commented out in this input list, but they can 
+#help to create a cleaner cluster main sequence, BUT at the expense of removing many cluster giants/subdwarfs/white dwarfs.
 clustering = hdbscan.HDBSCAN(min_cluster_size=100, min_samples=64).fit(fieldparscaled[["ra","dec","pmra","pmdec","parallax"]]) #,"phot_g_mean_mag","bp_rp"]])
 
 #The cluster selection and probability results, plus transformed RA, are added as new columns to the original dataframe.
@@ -69,4 +76,4 @@ fieldpar["M_G"]= fieldpar["phot_g_mean_mag"]-np.log10(fieldpar["distance"]/10)*5
 fieldpar[fieldpar["clusternum"]==extractnum].to_csv(clustername+"clustering.csv")
 
 #Plots the output clustered data.  See clusterplot.py for details.
-clusterplot(fieldpar)
+clusterplot(fieldpar,PlotMembershipProb)
--- clusterplot.py
+++ clusterplot.py
@@ -1,13 +1,34 @@
-def clusterplot(ClusteredData):
+def clusterplot(ClusteredData,MembershipPlot):
 	import seaborn as sns
 	import matplotlib.pyplot as plt
 	import scipy.stats as spy
 	import numpy as np
+	import pandas as pd
+	import matplotlib.colors as pltc
+
+#We count the number of found clusters, set up the color palette, and set tick label font size to 12 for clarity.
+
+	uniquegroup=ClusteredData.clusternum.unique()
+	palette = sns.color_palette('bright', max(uniquegroup)+1)
+	plt.rcParams['xtick.labelsize'] = 12
+	plt.rcParams['ytick.labelsize'] = 12
+
+#If plotting a membership probability color map is wanted, the input dataframe must be separated into a different dataframe for
+#each cluster.  Doing this with a dictionary is fast and efficient.  A second dictionary is made to set the color map for each cluster
+#number.  If plotting membership probability is not needed, we set a unique color to each cluster and set nonclustered field stars to black. 
+	if MembershipPlot==1:
+		ClusteredDict={elem: pd.DataFrame for elem in uniquegroup}
+		for key in ClusteredDict.keys():
+			ClusteredDict[key] = ClusteredData[:][ClusteredData.clusternum == key]
+		cmapdict={-1:"Greys",0:"Blues",1:"Oranges",2:"Greens",3:"Reds",4:"Purples",5:"PuBu",6:"YlGn",7:"Greys",8:"Purples",9:"Blues",10:"Greens",11:"Reds",12:"Oranges",13:"PuBu",14:"YlGn",15:"Greys"}
+		normalize = pltc.Normalize(vmin=0, vmax=1)
+	else:
+		colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in ClusteredData["clusternum"]]
 
 #This loop iterates through all clustered groups found with hdbscan and plots the requested clusters that pass the checks.
-	for step in range(max(ClusteredData["clusternum"])+1):
+	for step in range(max(uniquegroup)+1):
 
-#The median distance is found, which is used to transform proper motions and RA and DEC into spatial scales.
+#The median stellar distance is found (in parsecs), which is used to transform proper motions and RA and DEC into spatial scales.
 #Because these distributions are not always Gaussian, the interquartile ranges are used to analyze parameter variations throughout a cluster.
 		distcen=np.median(ClusteredData.loc[ClusteredData["clusternum"]==step,["distance"]])
 		pmraiqr=spy.iqr(ClusteredData.loc[ClusteredData["clusternum"]==step,["pmra"]])*3.14159/180*distcen*0.271795
@@ -15,18 +36,15 @@ def clusterplot(ClusteredData):
 
 #Hdbscan will typically create a cluster from the overdensity in the field disk stars, producing apparent clusters but with large PM variation.
 #Therefore, we require that the transformed PM variation be less than 3 km/s in each axis to be displayed as a valid cluster.  If a legitimate 
-#cluster is identified but retains a lot of noise/non-members, 3 km/s may remove it from display.  In these cases, you may increase the cut to 
-#4 or 5 km/s, etc., to first analyze its output, but in the end I recommend adjusting the hdbscan min_samples or the parameter scalings.
+#cluster is identified but retains a lot of noise/non-members, a 3 km/s cut may remove it from display.  In these cases, you may increase the 
+#cut to 4 or 5 km/s, etc., to first analyze its output, but in the end I recommend adjusting the hdbscan min_samples or the parameter scalings.
 		if (pmraiqr < 3 and pmdeciqr < 3):
 
-#To set up the plots, we set a separate Figure for each cluster.  We set up the color palette, set a color to each group, 
-#and set all clustered data to large size and nonclustered data to small data size.  Lastly, set tick label font size to 12 for clarity.
-			plt.figure(step,figsize=(12, 10))
-			palette = sns.color_palette('bright', np.unique(ClusteredData["clusternum"]).max() + 1)
-			colors = [palette[x] if x >= 0 else (0.0, 0.0, 0.0) for x in ClusteredData["clusternum"]]
-			sizes = [4 if x == step else 0.005 for x in ClusteredData["clusternum"]]
-			plt.rcParams['xtick.labelsize'] = 12
-			plt.rcParams['ytick.labelsize'] = 12
+#To set up the plots, we set a separate Figure for each cluster.  Forr when membership probability is not shown, we set all clustered data to 
+#a large size and nonclustered data to small data size.  Lastly, set tick label font size to 12 for clarity.
+			plt.figure(step,figsize=(14, 10))
+			if MembershipPlot != 1:
+				sizes = [4 if x == step else 0.003 for x in ClusteredData["clusternum"]]
 
 #A subplot illustrating the cluster distance histogram.  Additionally, the median cluster distance and distance IQR are plotted. 
 			plt.subplot(223)
@@ -40,9 +58,9 @@ def clusterplot(ClusteredData):
 			plt.gcf().text(0.09, 0.43, "Distance = "+str(int(distcen))+" pc", fontsize=12)
 			plt.gcf().text(0.32, 0.43, "Distance IQR = "+str(int(pmdistiqr))+" pc", fontsize=12)
 			
-#A subplot illustrating the cluster RA and DEC.  To attempt to place the polar coordinates onto a meaningful 2D plane, the RA are 
-#transformed relative to the central RA and scaled to the cosine of declination.  Lastly, the median cluster RA and DEC and their 
-#IQR in pc space are plotted. 
+#A subplot illustrating the cluster scaled RA and DEC.  The median cluster RA and DEC and their IQR in pc space are plotted. 
+#Whether or not the membership probability colormap was desired is checked, and a separate color map for each cluster is set 
+#or the simple setup is adopted.
 			plt.subplot(222)
 			plt.ylabel("DEC",fontsize=15)
 			plt.xlabel("RA Scaled",fontsize=15)
@@ -58,7 +76,18 @@ def clusterplot(ClusteredData):
 			deciqr=spy.iqr(ClusteredData.loc[ClusteredData["clusternum"]==step,["dec"]])*3.14159/180*distcen
 			plt.text(ramin, decmax+(decmax-decmin)*0.08, "DEC = "+str(round(decmedian,3)), fontsize=12)
 			plt.text(ramin+(ramax-ramin)*0.6, decmax+(decmax-decmin)*0.08, "DEC IQR = "+str(round(deciqr,2))+" pc", fontsize=12)
-			plt.scatter(ClusteredData["ratransform"],ClusteredData["dec"], s=sizes, alpha=1,color=colors)
+			if MembershipPlot==1:
+				for g in uniquegroup:
+					if g==-1:
+						plt.scatter(ClusteredDict[g]["ratransform"],ClusteredDict[g]["dec"],s=0.005, alpha=1,color="black")
+					elif g==step:
+						plt.scatter(ClusteredDict[g]["ratransform"],ClusteredDict[g]["dec"],s=6, alpha=1,c=ClusteredDict[g]["clusterprob"],cmap=cmapdict[g],norm=normalize)
+						cbar=plt.colorbar()
+						cbar.set_label('Membership Probability')
+					else:
+						plt.scatter(ClusteredDict[g]["ratransform"],ClusteredDict[g]["dec"], s=0.02, alpha=1,c=ClusteredDict[g]["clusterprob"],cmap=cmapdict[g],norm=normalize)				
+			else:
+				plt.scatter(ClusteredData["ratransform"],ClusteredData["dec"], s=sizes, alpha=1,color=colors)
 		
 #A subplot illustrating the cluster proper motions.  Additionally, the median cluster RA PM and DEC PM and their IQR in km/s space are plotted. 
 			plt.subplot(221)
@@ -72,7 +101,18 @@ def clusterplot(ClusteredData):
 			plt.gcf().text(0.28, 0.948, "DEC PM IQR = "+str(round(pmdeciqr,2))+" km/s", fontsize=12)
 			plt.ylim(pmdecmedian-10,pmdecmedian+10)
 			plt.xlim(pmramedian-10,pmramedian+10)
-			plt.scatter(ClusteredData["pmra"],ClusteredData["pmdec"], s=sizes, alpha=1,color=colors)
+			if MembershipPlot==1:
+				for g in uniquegroup:
+					if g==-1:
+						plt.scatter(ClusteredDict[g]["pmra"],ClusteredDict[g]["pmdec"],s=0.005, alpha=1,color="black")
+					elif g==step:
+						plt.scatter(ClusteredDict[g]["pmra"],ClusteredDict[g]["pmdec"],s=5, alpha=1,c=ClusteredDict[g]["clusterprob"],cmap=cmapdict[g],norm=normalize)
+						cbar=plt.colorbar()
+						cbar.set_label('Membership Probability')
+					else:
+						plt.scatter(ClusteredDict[g]["pmra"],ClusteredDict[g]["pmdec"], s=0.02, alpha=1,c=ClusteredDict[g]["clusterprob"],cmap=cmapdict[g],norm=normalize)				
+			else:
+				plt.scatter(ClusteredData["pmra"],ClusteredData["pmdec"], s=sizes, alpha=1,color=colors)
 			
 #A subplot illustrating the cluster CMD.  Additionally, the cluster number and the total count of cluster members are plotted.
 			plt.subplot(224)
@@ -83,9 +123,22 @@ def clusterplot(ClusteredData):
 			plt.ylim(ymax,ymin-1)
 			plt.xlim(-0.6,3.5)
 			number=len(ClusteredData.loc[ClusteredData["clusternum"]==step])
-			plt.text(1.95, ymin+0.5, "Cluster Number "+str(step), fontsize=12)
-			plt.text(1.95, ymin+1.68, "Cluster Count = "+str(number), fontsize=12)
-			plt.scatter(ClusteredData["bp_rp"],ClusteredData["M_G"], s=sizes, alpha=1,color=colors)
+			if MembershipPlot==1:
+				xlabelpos=1.57
+				for g in uniquegroup:
+					if g==-1:
+						plt.scatter(ClusteredDict[g]["bp_rp"],ClusteredDict[g]["M_G"],s=0.005, alpha=1,color="black")
+					elif g==step:
+						plt.scatter(ClusteredDict[g]["bp_rp"],ClusteredDict[g]["M_G"],s=5, alpha=1,c=ClusteredDict[g]["clusterprob"],cmap=cmapdict[g],norm=normalize)
+						cbar=plt.colorbar()
+						cbar.set_label('Membership Probability')
+					else:
+						plt.scatter(ClusteredDict[g]["bp_rp"],ClusteredDict[g]["M_G"], s=0.02, alpha=1,c=ClusteredDict[g]["clusterprob"],cmap=cmapdict[g],norm=normalize)				
+			else:
+				xlabelpos=1.95
+				plt.scatter(ClusteredData["bp_rp"],ClusteredData["M_G"], s=sizes, alpha=1,color=colors)
+			plt.text(xlabelpos, ymin+0.5, "Cluster Number "+str(step), fontsize=12)
+			plt.text(xlabelpos, ymin+1.68, "Cluster Count = "+str(number), fontsize=12)
 			plt.tight_layout()
 		
 	plt.show()
